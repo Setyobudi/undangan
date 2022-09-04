bad6d2313469d9122282e0a474451daa7e1cefeb
Muhammad Hakim Setyobudi <hakimlogorg@yahoo.co.id>
2022-09-05 02:23:50 +0700

    Update index.js

--- src/components/WishesSection/SendWishes/index.js
+++ src/components/WishesSection/SendWishes/index.js
@@ -87,7 +87,7 @@ function SendWishes( {urlCode, urlName, urlType} ) {
 
       <Fragment>
         <form name="wish" method="POST" enctype="multipart/form-data" data-netlify="true" netlify-honeypot="bot-field" hidden>
-        <input type="hidden" name="wish" value="wish"/>
+        
           <div className="row">
             <div className="col-md-4 col-md-offset-4">
               <div class="form-group">
